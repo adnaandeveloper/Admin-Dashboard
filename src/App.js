767751775a78ac9a95626bf777b3067fe148f3f7
amaydeveloper <amaydeveloper@MacBook-Pro-tilhrende-amaydeveloper.local>
2022-05-10 13:30:39 +0200

    user page is initiated and now its time for salaah duhr alxamdulillah

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './app.css';
 import Home from './pages/home/Home';
 import { Routes, Route } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
+import User from './pages/user/User';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
         </Routes>
       </div>
     </>
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './user.css';
+const User = () => {
+  return (
+    <div className="user">
+      <div className="userTitleContainer">
+        <h3 className="userTitle">Edit User</h3>
+        <button className="userAddButton">Create</button>
+      </div>
+    </div>
+  );
+};
+
+export default User;
